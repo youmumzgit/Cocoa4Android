b48a6ad2cba9e66f44f7d510a8c2ff7a4fabf2d3
wutong <youmumzyx@gmail.com>
2013-01-27 17:17:08 +0800

    UIWebDelegate added

--- Cocoa4Android/src/org/cocoa4android/ui/UIWebView.java
+++ Cocoa4Android/src/org/cocoa4android/ui/UIWebView.java
@@ -16,12 +16,15 @@
 package org.cocoa4android.ui;
 
 import org.cocoa4android.cg.CGRect;
+import org.cocoa4android.ns.NSError;
 import org.cocoa4android.ns.NSURLRequest;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.MotionEvent;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
@@ -31,6 +34,14 @@ public class UIWebView extends UIView {
 	
 	private boolean enableMutipleScroll = NO;
 	
+	private UIWebViewDelegate delegate;
+	
+	public UIWebViewDelegate delegate() {
+		return delegate;
+	}
+	public void setDelegate(UIWebViewDelegate delegate) {
+		this.delegate = delegate;
+	}
 	public boolean isEnableMutipleScroll() {
 		return enableMutipleScroll;
 	}
@@ -46,6 +57,8 @@ public class UIWebView extends UIView {
 	}
 	public UIWebView(){
 		webView = new ResizableWebView(context);
+		CocoaWebViewClient client = new CocoaWebViewClient();
+		webView.setWebViewClient(client);
 		this.setView(webView);
 		
 	}
@@ -158,5 +171,36 @@ public class UIWebView extends UIView {
 	public interface WebViewSizeChangeLisener{
 		public void webViewDidChangeSize(float width,float height);
 	}
+	
+	
+	public interface UIWebViewDelegate{
+		public void webViewDidFinishLoad(UIWebView webView);
+		public void webViewDidStartLoad(UIWebView webView);
+		public void webViewDidFailLoadWithError(UIWebView webView,NSError error);
+	}
+	
+	public class CocoaWebViewClient extends WebViewClient {
+		  @Override
+		  public void onPageStarted(WebView view, String url, Bitmap favicon) {
+		     if (UIWebView.this.delegate!=null) {
+		    	 UIWebView.this.delegate.webViewDidStartLoad(UIWebView.this);
+		     }
+		  }
+		  @Override
+		  public void onPageFinished(WebView webView, String url) {
+			  if (UIWebView.this.delegate!=null) {
+			    	 UIWebView.this.delegate.webViewDidFinishLoad(UIWebView.this);
+			  }
+		  }
+		  @Override
+		  public void onReceivedError(WebView view, int errorCode,
+		            String description, String failingUrl) {
+			  if (UIWebView.this.delegate!=null) {
+				  //FIXME nserror description
+			    	 UIWebView.this.delegate.webViewDidFailLoadWithError(UIWebView.this,NSError.errorWithDomain("", errorCode,null));
+			  }
+		   }
+		  
+		}
 	
 }
